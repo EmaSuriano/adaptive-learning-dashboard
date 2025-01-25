cee6dfe10d136ded41cfe26f587ce3f1c57e924a
EmaSuriano <emanuel.suriano@gmail.com>
2025-01-25 12:22:14 +0100

    add proper sitre

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
-const GH_REPO_PATH = "https://github.com/EmaSuriano/adaptive-learning-dashboard"
+const GH_REPO_PATH = 'https://emasuriano.github.io/adaptive-learning-dashboard/'
 
 const base = process.env.NODE_ENV === 'production' ? GH_REPO_PATH : ''
 
