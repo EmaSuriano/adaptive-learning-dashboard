3c9465f68989b2aca43f2fbe270b2d9a0f584702
EmaSuriano <emanuel.suriano@gmail.com>
2025-01-25 12:17:33 +0100

    add base for ci

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "https://github.com/EmaSuriano/adaptive-learning-dashboard"
   plugins: [react()],
   resolve: {
     alias: {
